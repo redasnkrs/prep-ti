38c5190533776e9ac21bdf3c3b9d611c2bde87b9
redasnkrs <redadahmani34@gmail.com>
2025-02-18 15:04:29 +0100

    added the title for all the pages

--- public/index.php
+++ public/index.php
@@ -6,19 +6,24 @@
 if (isset($_GET['p'])) {
   switch ($_GET['p']) {
     case "aboutme":
+      $title = "apropos";
       include "../templates/aproposView.php";
       break;
     case "history":
+      $title = "histoire";
       include "../templates/histoireView.php";
       break;
     case "contact":
+      $title = "contact";
       include "../templates/contactView.php";
       break;
     default:
+      $title = "erreur404";
       include "../templates/erreur404View.php";
       break;
   }
 } else {
+  $title = "accueil";
   //  Default view : 
   include "../templates/acceuilView.php";
 }
